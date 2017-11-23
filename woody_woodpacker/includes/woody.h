4c62a8a7cbeab94fa20267cba477ca730ae2d858
Tom KTORZA <tktorza@e3r9p14.42.fr>
2017-11-23 16:20:25 +0100

    for compill

--- woody_woodpacker/includes/woody.h
+++ woody_woodpacker/includes/woody.h
@@ -6,7 +6,7 @@
 /*   By: tktorza <tktorza@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 16:37:57 by tktorza           #+#    #+#             */
-/*   Updated: 2017/11/17 15:45:53 by tktorza          ###   ########.fr       */
+/*   Updated: 2017/11/23 16:20:13 by tktorza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 # include "../libft/inc/libft.h"
 # include "../libft/inc/ft_printf.h"
 
-void	woody_start(void *ptr, unsigned int size);
+void    woody_start(void *ptr, unsigned int size, int fd);
 
 #endif
