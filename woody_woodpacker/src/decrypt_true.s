c68f25fe93790a1be4e6bb630a125425bbeb4ebe
Tom KTORZA <tktorza@e3r6p4.42.fr>
2018-03-26 15:32:52 +0200

    change for compile on VM

--- woody_woodpacker/src/decrypt_true.s
+++ woody_woodpacker/src/decrypt_true.s
@@ -1,33 +1,19 @@
 section .text
 global _decrypt_true
-extern _ft_putnbr
-extern _ft_putchar
 ;RDI,   RSI,   RDX, RCX, R8, R9, XMM0–7
 ;data, offset, size, key, index
 
-_decrypt_true:
-    ;enter 16, 0
-    mov r13, rdx
-	lea rdi, [rdi + rsi]
+_start:
+    mov r13, 0x33333333
+	lea rdi, [0x22222222]
     mov bl, 1
 
-;loop:
-;    cmp BYTE [rdi], 1
-;    jb increment
-;
 looping:
-;    cmp BYTE[rdi], 255
-;    jae increment
     sub BYTE [rdi], bl
 
 increment:
-    ;lea rdi, [rdi + 1]
     inc rdi
     add rsi, 1
     cmp rsi, r13 
     jne looping
-
-return:
-    ;leave
-    ret
-    ;jmp 0x11111111
+    jmp 0x11111111
